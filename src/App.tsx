07ca10c1cb4ef25e39decdee4167f9d8244a5ac2
Connor <connor5741213@gmail.com>
2023-03-20 22:47:26 -0400

    homepage with routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import './App.css'
+import Home from "./components/Home/Home"
 import Contact from "./components/Contact/Contact";
 import Explore from "./components/Explore/Explore";
 import Login from "./components/Login/Login";
@@ -9,10 +10,10 @@ function App() {
       <Router>
         {/* <NavBar /> */}
         <Routes>
-          <Route  path="/" element={""} />
-          <Route  path="/login" element={<Login />} />
-          <Route  path="/explore" element={<Explore />} />
-          <Route  path="/contact" element={<Contact />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/explore" element={<Explore />} />
+          <Route path="/contact" element={<Contact />} />
         </Routes>
       </Router>
     </div>
